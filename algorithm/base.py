d763eb81fe6725c0f348e69a697c4df1db109a84
Nian Ran <992988188@qq.com>
2025-06-11 14:32:39 +0800

    delete

--- algorithm/base.py
+++ algorithm/base.py
@@ -21,10 +21,10 @@ class Item:
             if p in ['qed','jnk3','bbbp1']:
                 '''
                  'celecoxib_rediscovery', 'troglitazone_rediscovery','thiothixene_rediscovery',
-              'albuterol_similarity','mestranol_similarity',
-             'isomers_c7h8n2o2','isomers_c9h10n2o2pf2cl','median1','median2', 'osimertinib_mpo',
-             'fexofenadine_mpo','ranolazine_mpo','perindopril_mpo', 'amlodipine_mpo',
-             'sitagliptin_mpo','zaleplon_mpo','valsartan_smarts', 'deco_hop', 'scaffold_hop'
+                'albuterol_similarity','mestranol_similarity',
+                'isomers_c7h8n2o2','isomers_c9h10n2o2pf2cl','median1','median2', 'osimertinib_mpo',
+                'fexofenadine_mpo','ranolazine_mpo','perindopril_mpo', 'amlodipine_mpo',
+                'sitagliptin_mpo','zaleplon_mpo','valsartan_smarts', 'deco_hop', 'scaffold_hop'
                 '''
                 self.total += self.property[p]
             elif p == 'sa':
